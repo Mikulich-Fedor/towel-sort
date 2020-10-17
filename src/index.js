f47b29cfd09863c8eddb5a56a8b546bee2d5e511
fiodar miculich <feotheodor@gmail.com>
2020-10-17 15:36:39 +0300

    fix: fixing funk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@
 
 module.exports = function towelSort (matrix) {
   if(Array.isArray(matrix)){
-    let arr = []
-    arr = matrix.flat(Infinity);
-    arr = arr.sort((a,b) => a - b);
-    arr = arr.filter((item, index) => arr.indexOf(item) === index);
-    return arr;
-  }else{
-    return false;
+    let arr;    
+    return arr = matrix.map((item,index) => !(index & 1) ? item.sort((a,b) => a - b) : item.sort((a,b) => b - a)).flat(Infinity);
+  }
+  else{
+    return [];
   }
 }
